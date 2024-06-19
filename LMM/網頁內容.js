ddfdd34fd61eecbc1b2ea5b5440530ce5dabd28c
Kumaaaaaaaaaaaakuaaaaa <161785768+Kumaaaaaaaaaaaakuaaaaa@users.noreply.github.com>
2024-06-19 23:32:58 +0800

    Update 網頁內容.js

--- LMM/網頁內容.js
+++ LMM/網頁內容.js
@@ -3,7 +3,21 @@ const main = document.querySelector('main')
 let key = "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"
 
 async function groqChat(q) {
-
+   if (!q.trim()) { 
+        return "請輸入問題 感謝您! 可以嘗試輸入: 心情不好 or 如何與女友開話題 or 世界上最可愛的狗在哪?";
+    }
+    try{
+        if(q.trim()=="心情不好"){
+            return "別擔心 沒有什麼事是一支冰無法解決的! 不行的話就兩支~";
+        }
+        else if(q.trim()=="如何與女友開話題"){
+            return "現在馬上傳給她 暖她一整天 她就會問你：她是誰? 於是你們之間就會有新的話題可以聊了";
+        }
+        else if(q.trim()=="世界上最可愛的狗在哪?"){
+            return "在我家";
+        }
+   
+    
     const jsonResponse = await fetch("https://api.groq.com/openai/v1/chat/completions", 
     {
         body: JSON.stringify({
