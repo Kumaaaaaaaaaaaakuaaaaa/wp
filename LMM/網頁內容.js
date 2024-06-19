b80e7c8f20ccd3e2881bf5079c8226e3326c811e
Kumaaaaaaaaaaaakuaaaaa <161785768+Kumaaaaaaaaaaaakuaaaaa@users.noreply.github.com>
2024-06-19 22:44:00 +0800

    Update 網頁內容.js

--- LMM/網頁內容.js
+++ LMM/網頁內容.js
@@ -1,27 +1,31 @@
-// 獲取頁面元素
-const qNode = document.querySelector('#question');
-const responseNode = document.querySelector('#response');
+const main = document.querySelector('main')
 
-import Groq from "groq-sdk";
+let key = "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"
 
-const groq = new Groq({ apiKey:	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 });
-
-export async function main() {
-  const chatCompletion = await getGroqChatCompletion();
-  // Print the completion returned by the LLM.
-  console.log(chatCompletion.choices[0]?.message?.content || "");
+async function groqChat(q) {
+    const jsonResponse = await fetch("https://api.groq.com/openai/v1/chat/completions", 
+    {
+        body: JSON.stringify({
+            "model": "llama3-8b-8192",
+            "messages": [{"role": "user", "content": q}],
+            "temperature": 0.7
+        }),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${key}`,
+        }
+    })
+    const jsonData = await jsonResponse.json()
+    console.log(JSON.stringify(jsonData, null, 2))
+    return jsonData.choices[0].message.content
 }
 
-export async function getGroqChatCompletion() {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: "Explain the importance of fast language models",
-      },
-    ],
-    model: "llama3-8b-8192",
-  });
-}
 
-});
+async function chat() {
+    let qNode = document.querySelector('#question')
+    let responseNode = document.querySelector('#response')
+    responseNode.innerText = '詢問 ChatGPT 中，請稍等幾秒鐘 ...'
+    let answer = await groqChat(qNode.value)
+    responseNode.innerText = answer
+}
